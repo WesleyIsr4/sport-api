ab0ab9562ebce392843994db06838c78aa6efe99
Wesley Israel Nogueira da Silva <wesley@pop-os.localdomain>
2022-11-15 13:38:52 -0300

    :bug: fix services api

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: 'https://api-football-v1.p.rapidapi.com/v3/',
   headers: {
-    'X-RapidAPI-Key': '0281c8698dmsh60d8c916561e88bp1b0429jsnaacc2158fc71',
+    'X-RapidAPI-Key': process.env.RAPID_KEY,
     'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com',
   },
 });
